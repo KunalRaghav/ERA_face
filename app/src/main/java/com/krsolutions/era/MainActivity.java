e2201dd4bbceb150f7113e6f692ecd6c60dee188
KunalRaghav <kraghav123@gmail.com>
2019-05-04 15:46:41 +0530

    added camera2api

--- app/src/main/java/com/krsolutions/era/MainActivity.java
+++ app/src/main/java/com/krsolutions/era/MainActivity.java
@@ -3,12 +3,22 @@ package com.krsolutions.era;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, CameraFragment.newInstance())
+                    .commit();
+        }
     }
 }
